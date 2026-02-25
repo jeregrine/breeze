81e0a41a500a4fae42fbabd019a61d625420bc0d
Jason Stiebs <stiebs.81@gmail.com>
2026-02-25 14:34:37 -0600

    test(Breeze.Template): expand template and formatter coverage
    Add focused tests for the native ~H runtime:
    - interpolation + EEx expressions
    - :if/:for directives
    - dynamic/boolean/spread attrs
    - components, inner_block, named slots, private components
    - template compile-time error cases
    
    Also broaden Breeze.HTMLFormatter tests for slot formatting,
    EEx preservation, and boolean/spread attribute layout.

--- test/breeze/html_formatter_test.exs
+++ test/breeze/html_formatter_test.exs
@@ -40,6 +40,51 @@ defmodule Breeze.HTMLFormatterTest do
              """
   end
 
+  test "formats slot blocks with directives" do
+    source = "<.list><:item :for={x <- @items} value={x}><box>{x}</box></:item></.list>"
+
+    assert HTMLFormatter.format(source,
+             sigil: :H,
+             opening_delimiter: "\"\"\"",
+             line_length: 40
+           ) ==
+             """
+             <.list>
+               <:item :for={x <- @items} value={x}>
+                 <box>{x}</box>
+               </:item>
+             </.list>
+             """
+  end
+
+  test "keeps eex expressions and assign syntax" do
+    source = "<box><%= String.upcase(@name) %></box>"
+
+    assert HTMLFormatter.format(source, sigil: :H, opening_delimiter: "\"\"\"") ==
+             """
+             <box><%= String.upcase(@name) %></box>
+             """
+  end
+
+  test "formats boolean and spread attributes" do
+    source = "<box focusable id={@id} {@rest}>x</box>"
+
+    assert HTMLFormatter.format(source,
+             sigil: :H,
+             opening_delimiter: "\"\"\"",
+             line_length: 20
+           ) ==
+             """
+             <box
+               focusable
+               id={@id}
+               {@rest}
+             >
+               x
+             </box>
+             """
+  end
+
   test "supports ~H\"...\" noformat modifier" do
     source = "<box><box>{@name}</box></box>"
     assert HTMLFormatter.format(source, sigil: :H, modifiers: ~c"noformat") == source
