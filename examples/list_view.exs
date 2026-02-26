84ddf5b91b9aada14851431856de3e3a1eeec02c
Jason Stiebs <stiebs.81@gmail.com>
2026-02-26 09:39:53 -0600

    docs(examples): document viewport helpers and list view API
    - refresh Breeze.View implicit docs for init/3 and handle_modifiers/3
    - document structured viewport scroll modifiers and Breeze.ListView in README
    - update scroll/docs examples to use scroll_y + Breeze.Viewport helpers
    - add a dedicated list_view example using Breeze.ListView

---
+++ examples/list_view.exs
@@ -0,0 +1,64 @@
+defmodule ListViewDemo do
+  use Breeze.View
+
+  def mount(_opts, term) do
+    {:ok, term |> focus("languages") |> assign(selected: nil)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <box>
+      <.list id="languages" br-change="change">
+        <:item value="elixir">Elixir</:item>
+        <:item value="erlang">Erlang</:item>
+        <:item value="rust">Rust</:item>
+        <:item value="go">Go</:item>
+        <:item value="zig">Zig</:item>
+        <:item value="python">Python</:item>
+        <:item value="lua">Lua</:item>
+        <:item value="gleam">Gleam</:item>
+        <:item value="haskell">Haskell</:item>
+      </.list>
+
+      <box style="border width-24" :if={@selected}>Selected: <%= @selected %></box>
+    </box>
+    """
+  end
+
+  attr(:id, :string, required: true)
+  attr(:rest, :global)
+
+  slot :item do
+    attr(:value, :string, required: true)
+  end
+
+  def list(assigns) do
+    ~H"""
+    <box
+      id={@id}
+      implicit={Breeze.ListView}
+      list-loop="true"
+      list-scroll-padding="1"
+      focusable
+      style="border width-24 height-8 overflow-hidden focus:border-3"
+      {@rest}
+    >
+      <box
+        :for={item <- @item}
+        value={item.value}
+        style="selected:bg-4 selected:text-7 width-24"
+      ><%= render_slot(item, %{}) %></box>
+    </box>
+    """
+  end
+
+  def handle_event("change", %{value: value}, term) do
+    {:noreply, assign(term, selected: value)}
+  end
+
+  def handle_info(_, term), do: {:noreply, term}
+  def handle_event(_, _, term), do: {:noreply, term}
+end
+
+Breeze.Server.start_link(view: ListViewDemo)
+:timer.sleep(100_000)
